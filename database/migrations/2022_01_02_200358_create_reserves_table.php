d0854f35431ae0acf9a4661ca15e2f34140ba6e3
Phelipe Stering <phelipe.stering@hotmail.com>
2022-01-02 20:36:55 -0300

    AULA: 04 - MIGRATIONS (PARTE FINAL) - CONCLUINDO AS MIGRATIONS

---
+++ database/migrations/2022_01_02_200358_create_reserves_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reserves', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('flight_id')->unsigned();
+            $table->date('data_reserved');
+            $table->enum('status',[
+                'reserved',
+                'canceled',
+                'paid',
+                'concluded'
+            ]);
+
+//CHAVES estrangeiras
+
+            $table->foreign('user_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+
+            $table->foreign('flight_id')
+                    ->references('id')
+                    ->on('flights')
+                    ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reserves');
+    }
+}
