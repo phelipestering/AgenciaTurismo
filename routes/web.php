2e382bbb93cf465986f4490abf03acdda7037fd5
Phelipe Stering <phelipe.stering@hotmail.com>
2022-01-03 00:00:35 -0300

    AULA: 07 - HOME PAGE DASHBOARD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PanelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/panel', [PanelController::class, 'index'])->name('panel.index');
+
