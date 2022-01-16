af611c7f4a647dbb72c9985dea25de6db2e14746
PHELIPE STERING <68488889+phelipestering@users.noreply.github.com>
2022-01-15 23:52:08 -0300

    INCLUDE ERRORS VIEW CREATE EDIT / BRANDS

--- resources/views/panel/brands/create-edit.blade.php
+++ resources/views/panel/brands/create-edit.blade.php
@@ -17,13 +17,7 @@
 
         @if (isset($errors) && $errors -> any ())
 
-            <div class="alert alert-warning">
-                <ul>
-                    @foreach ($errors->all() as $error )
-                        </li>{{ $error }}<li>
-                    @endforeach
-                </ul>
-            </div>
+        @include('panel.includes.errors')
 
         @endif
 
---
+++ resources/views/panel/includes/errors.blade.php
@@ -0,0 +1,7 @@
+<div class="alert alert-warning">
+    <ul>
+        @foreach ($errors->all() as $error )
+            </li>{{ $error }}<li>
+        @endforeach
+    </ul>
+</div>
